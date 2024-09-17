a325d102b0ad22dd5ba46eaf39bae35137c7b8c1
Mahadi Hasan <muhammadmahdihasan1@gmail.com>
2024-09-17 22:59:52 +0600

    working on auth

--- src/components/infobar/index.tsx
+++ src/components/infobar/index.tsx
@@ -11,6 +11,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { UserButton } from "@clerk/nextjs";
 
 type Props = {};
 
@@ -68,7 +69,7 @@ const InfoBar = (props: Props) => {
           </TooltipContent>
         </Tooltip>
       </TooltipProvider>
-      {/* <UserButton /> */}
+      <UserButton />
     </div>
   );
 };
